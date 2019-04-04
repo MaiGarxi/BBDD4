4f5717a5d766e9d08af0efea1af9c10b230c6055
MaiGarxi <46573115+MaiGarxi@users.noreply.github.com>
2019-04-04 19:21:00 +0200

    conexion establecida

--- BBDD/src/bbdd/Conectar.java
+++ BBDD/src/bbdd/Conectar.java
@@ -15,7 +15,6 @@ public class Conectar {
         Class.forName("com.mysql.jdbc.Driver");
               conectar=DriverManager.getConnection("jdbc:mysql://localhost/reto4","root","");
                 System.out.println("Conexion establecida");
-                JOptionPane.showMessageDialog(null,"conexion establecida");
                 }
         catch(Exception e){
           System.out.println(e.getMessage());}
